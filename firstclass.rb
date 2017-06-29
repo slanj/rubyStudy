73f3dcafc748a8ddb31e75717938da14244dbd86
Slanj <slanjr@gmail.com>
2017-06-29 12:52:38 +0300

    repeat block

--- firstclass.rb
+++ firstclass.rb
@@ -1,9 +1,13 @@
 class Sample
   def hello
-    puts "Hello, Class! "
+    puts "Hello, Class!"
   end
 end
 
 s = Sample.new
 
 5.times {s.hello}
+
+3.times do |i|
+  puts "#{i}: Hello, World!"
+end
