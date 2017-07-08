ad24edd862863ab39fd692a920a410de9ceb31d1
slanj <slanjr@gmail.com>
2017-07-08 12:56:20 +0300

    cat talks

--- cotoclass.rb
+++ cotoclass.rb
@@ -1,9 +1,23 @@
 class Cat
   def talk
-    puts "Люди - мои слуги"
+    r = rand(5)
+    if r.zero?
+      puts 'Люди - мои слуги'
+    elsif r == 1
+      puts 'Покорми меня!'
+    elsif r == 2
+      puts 'Я наблевала в ванной'
+    elsif r == 3
+      puts 'Вот я - кот, а ты чего добился в жизни?'
+    elsif r == 4
+      puts 'Возьми на руки котика!'
+    end
   end
 
   def polezhat(mesto)
     puts "Иду лежать на #{mesto}"
   end
 end
+
+skvo = Cat.new
+skvo.talk
