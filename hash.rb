4d4aa3a45a3453c9e523f9409574c754985e5bb9
Slanj <slanjr@gmail.com>
2017-07-12 13:05:02 +0300

    hash

---
+++ hash.rb
@@ -0,0 +1,9 @@
+puts "Enter some text: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by {|a, b| b }
+frequencies.reverse!
+frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
