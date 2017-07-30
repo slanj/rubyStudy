d154ad126570cb2c30b64fac4f6da66aab23aa3f
Slanj <slanjr@gmail.com>
2017-07-30 17:37:08 +0300

    private and public banking

---
+++ banking.rb
@@ -0,0 +1,38 @@
+class Account
+	attr_reader :name, :balance
+
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  public
+  def display_balance(pin_number)
+  	if pin_number == pin
+      puts "Balance: $#{@balance}"
+    else
+      puts pin_error
+    end
+  end
+
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else
+      puts pin_error
+    end
+  end
+
+  private
+  def pin
+    @pin = 1234
+  end
+
+  def pin_error
+    'Access denied: incorrect PIN.'
+  end
+end
+
+acc = Account.new("Andrew")
+acc.withdraw(1234, 42)
