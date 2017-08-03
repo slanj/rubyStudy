11dd038ccffa91403106b8d055bc169c57fc6159
Slanj <slanjr@gmail.com>
2017-08-03 11:45:29 +0300

    array method redefine

---
+++ array_redefine.rb
@@ -0,0 +1,13 @@
+class Array
+  def each
+    index = 0
+    while index < self.length
+      yield self[index]
+      index += 1
+      puts 'Woohoo!'
+    end
+  end
+end
+
+a = [1, 2, 3]
+a.each { |item| puts item }
