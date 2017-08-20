0d0ebb8329a73cdd82e29cf677a1765dee3639b7
Slanj <slanjr@gmail.com>
2017-08-20 08:54:32 +0000

    new resource

--- railsample/config/routes.rb
+++ railsample/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :hotels
   resources :stories
   get 'welcome/index'
--- railsample/db/schema.rb
+++ railsample/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817101849) do
+ActiveRecord::Schema.define(version: 20170820084440) do
 
   create_table "hotels", force: :cascade do |t|
     t.string "title"
     t.text "review"
     t.integer "rating"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
